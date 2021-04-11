2cb78afd38beb4b4063adb94c7764ca94596ab71
BENBRIKMouad <61920384+BENBRIKMouad@users.noreply.github.com>
2021-04-11 14:53:15 +0000

    Update 7-puppet_install_nginx_web_server.pp

--- 0x02-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x02-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,16 +2,16 @@
 package { 'nginx':
   ensure => installed,
 }
+file { '/usr/share/nginx/html/index.html':
+  ensure  => 'present',
+  content => 'Holberton School',
+}
 
-file_line { 'aaaaa':
+file_line { 'redirect_me':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-}
-
-file { '/var/www/html/index.html':
-  content => 'Holberton School',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=8s22PZRXAIY permanent;',
 }
 
 service { 'nginx':
