b82a8cec7b3dab736735df7ccabcbbdd4a1db6ba
BENBRIKMouad <61920384+BENBRIKMouad@users.noreply.github.com>
2021-04-18 18:04:32 +0000

    Update 2-puppet_custom_http_response_header.pp

--- 0x03-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x03-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,13 +1,13 @@
-# pupet to install nginx
+#!/usr/bin/env bash
 package { 'nginx':
   ensure => installed,
 }
 
-file_line { 'redirect_me':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'add_header X-Served-By $HOSTNAME;',
+exec { 'http header':
+  command  => '
+	sudo sed -i "/server_name _/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default
+	sudo service nginx restart',
+  provider => shell,
 }
 
 file { '/var/www/html/index.nginx-debian.html':
